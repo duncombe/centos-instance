4fd13f2f028ee1388c56b0201a7833ad108ce217
Christopher Duncombe Rae <christopher.duncombe.rae@noaa.gov>
2014-10-17 18:49:12 +0000

    Begin a set script.

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+# install this script by running 
+# git clone ssh://git@github.com/duncombe/centos-instance.git
+# cd centos instance
+# ./setup.sh
+
+## these commands are run 
+## test if these are already set: 
+## 	.gitconfig exists?
+## 	git config --global contains them?
+#git config --global user.name "Christopher Duncombe Rae"
+#git config --global user.email christopher.duncombe.rae@noaa.gov
+#
+## clone dotfiles repo and link them up to the local dotfiles   
+#git clone ssh://git@github.com/duncombe/dotfiles.git
+#git clone ssh://git@github.com/duncombe/centos-instance.git
+#
+## test if anaconda is installed else run
+#wget http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.1.0-Linux-x86_64.sh
+#bash Anaconda-2.1.0-Linux-x86_64.sh
+#
+## test if hugo is installed else run 
+#wget https://github.com/spf13/hugo/releases/download/v0.12/hugo_0.12_linux_amd64.tar.gz
+
+
